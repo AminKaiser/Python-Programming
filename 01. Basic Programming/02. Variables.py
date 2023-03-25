16f137e1764ebb619ace4174209645e67fbe1ba0
AminKaiser <aminkaiser90@gmail.com>
2023-03-25 16:32:38 +0600

    variables

---
+++ 01. Basic Programming/02. Variables.py
@@ -0,0 +1,28 @@
+student_count = 1000
+grade = 3.69
+is_passed = True
+course_name = "Python Programming"
+print(student_count)
+
+'''
+In Python, you don't need to explicitly declare variables before using them. 
+When you assign a value to a variable, Python creates the variable for you.
+However, there are some rules that you should follow when naming variables in Python:
+
+1. Variable names must start with a letter or underscore (_), but not with a number.
+2. Variable names can only contain letters, numbers, and underscores.
+3. Variable names are case sensitive. For example, "myVar" and "myvar" are two different variables.
+4. You cannot use reserved keywords as variable names, such as "if," "while," "for," "and," "or," "not," and "else."
+5. It's a good practice to use descriptive and meaningful variable names, so it's easy to understand the purpose of the variable.
+'''
+
+'''
+my_var = 5
+myVar = 6
+_myvar = 7
+
+1var = 5 # variable name cannot start with a number
+my-var = 6 # variable name cannot contain hyphen
+if = 7 # variable name cannot be a reserved keyword
+
+'''
