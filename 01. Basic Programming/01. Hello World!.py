943f77fa9aa535e1cfac5ccf7f2707cea004086a
AminKaiser <aminkaiser90@gmail.com>
2023-03-25 16:19:49 +0600

    Multiple line comment

--- 01. Basic Programming/01. Hello World!.py
+++ 01. Basic Programming/01. Hello World!.py
@@ -2,6 +2,14 @@ print("Hello World!")
 print("Amin Kaiser \n" * 10)
 print(2+2)
 # Formatting Documents
+# For Formatting Documents:
+#     Command Pallete > Format Documents
+#     x=1
+#     After Formatting
+#     x = 1
+
+#     #If we want to format the document when we save the document.
+#         >File>Preferences>Setting>On Search Bar- Format on Save> Tick This.
 x = 1
 x = -1
 y = 1
