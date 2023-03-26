3da59023f962f00ae952caaca2319e5aa9f8925c
AminKaiser <aminkaiser90@gmail.com>
2023-03-26 23:30:31 +0600

    numbers

---
+++ 01. Basic Programming/07. Numbers.py
@@ -0,0 +1,47 @@
+'''
+In Python, numbers are a built-in data type that can be used to represent numeric values. There are three main types of numbers in Python:
+
+Integers: Integers are whole numbers, such as -2, -1, 0, 1, 2, etc. In Python 3, integers have no size limit and can be as large as your system's memory allows.
+
+Floats: Floats are real numbers with a decimal point, such as 3.14, -0.5, 1.0, etc. Floats are stored using a fixed amount of memory, which means that they have a limited precision.
+
+Complex numbers: Complex numbers are numbers with a real part and an imaginary part, such as 1+2j, 2-3j, etc.
+
+Python provides various built-in functions and operators that can be used to perform arithmetic operations with numbers. Here are some examples:
+'''
+
+# Addition
+a = 5
+b = 3
+c = a + b
+print(c) # output: 8
+
+# Subtraction
+a = 5
+b = 3
+c = a - b
+print(c) # output: 2
+
+# Multiplication
+a = 5
+b = 3
+c = a * b
+print(c) # output: 15
+
+# Division
+a = 5
+b = 3
+c = a / b
+print(c) # output: 1.6666666666666667
+
+# Exponentiation
+a = 2
+b = 3
+c = a ** b
+print(c) # output: 8
+
+# Modulo
+a = 5
+b = 3
+c = a % b
+print(c) # output: 2
