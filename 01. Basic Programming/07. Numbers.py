8457d9205a486c87f0eff8df905806587a85ea36
AminKaiser <aminkaiser90@gmail.com>
2023-03-26 23:39:28 +0600

    numbers

--- 01. Basic Programming/07. Numbers.py
+++ 01. Basic Programming/07. Numbers.py
@@ -9,39 +9,81 @@ Complex numbers: Complex numbers are numbers with a real part and an imaginary p
 
 Python provides various built-in functions and operators that can be used to perform arithmetic operations with numbers. Here are some examples:
 '''
+# Integers
+x = 5
+y = -10
+print(x + y)  # Output: -5
+
+# Floating-point
+a = 3.14
+b = 2.5
+print(a * b)  # Output: 7.85
+
+# Complex
+z = 2 + 3j
+print(z.real)  # Output: 2.0
+print(z.imag)  # Output: 3.0
 
 # Addition
 a = 5
 b = 3
 c = a + b
-print(c) # output: 8
+print(c)  # output: 8
 
 # Subtraction
 a = 5
 b = 3
 c = a - b
-print(c) # output: 2
+print(c)  # output: 2
 
 # Multiplication
 a = 5
 b = 3
 c = a * b
-print(c) # output: 15
+print(c)  # output: 15
 
 # Division
 a = 5
 b = 3
 c = a / b
-print(c) # output: 1.6666666666666667
+print(c)  # output: 1.6666666666666667
 
 # Exponentiation
 a = 2
 b = 3
 c = a ** b
-print(c) # output: 8
+print(c)  # output: 8
 
 # Modulo
 a = 5
 b = 3
 c = a % b
-print(c) # output: 2
+print(c)  # output: 2
+
+# // Floor Division Operation
+'''
+In Python, the double forward-slash symbol (//) is used as the floor division operator. 
+It divides the left operand by the right operand and rounds down to the nearest integer.
+
+Here's an example:
+'''
+a = 10
+b = 3
+result = a // b
+print(result)   # Output: 3
+
+'''
+In the above code, a is divided by b using the floor division operator //. 
+Since 3 goes into 10 three times with a remainder of 1, the result is 3.
+
+Note that if both operands are integers, the result will also be an integer.
+If one or both operands are floating-point numbers, the result will be a floating-point number after 
+the division is performed.
+
+'''
+
+'''
+Python also provides built-in functions such as abs(), round(), min(), and max() 
+that you can use to perform various operations on numbers.
+Here are some examples:
+'''
