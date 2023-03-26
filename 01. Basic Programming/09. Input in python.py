8036aaf0b7ba66bafc8913707f8240f1056d1f93
AminKaiser <aminkaiser90@gmail.com>
2023-03-26 23:56:31 +0600

    input taking

---
+++ 01. Basic Programming/09. Input in python.py
@@ -0,0 +1,9 @@
+'''
+n Python, there are several ways to take user input from the command line or console. 
+Here are some common methods:
+
+Using the input() function: The input() function is used to read input from the user as a string.
+The syntax is as follows:
+
+'''
+user_input = input("Enter your input: ")
