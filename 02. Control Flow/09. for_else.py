32b9991742455e41fc6aeea53df3c905b157f0cf
AminKaiser <aminkaiser90@gmail.com>
2023-04-06 22:53:33 +0600

    for_else

---
+++ 02. Control Flow/09. for_else.py
@@ -0,0 +1,15 @@
+'''
+The for...else loop in Python is used to execute a block of code 
+when the loop has finished iterating over all the items in a sequence. 
+The else block will be executed if the loop completes all iterations without encountering a break statement.
+
+Here's the basic syntax of a for...else loop in Python:
+
+'''
+# for variable in sequence:
+#     # code to be executed inside the loop
+#     if condition:
+#         # code to be executed if condition is true
+#         break
+# else:
+#     # code to be executed if the loop completes all iterations without encountering a 'break' statement
